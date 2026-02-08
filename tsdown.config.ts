8ff636ea26bb7ea42cb20f37ce9de1de25a9c0b2
MarcoCaamal <caamalmarco99@gmail.com>
2026-02-07 22:41:08 -0500

    chore: update TypeScript target to ES2024 and Node.js target to node24

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/index.ts'],
-  target: 'node23', // O la versión de Node que uses
+  target: 'node24', // O la versión de Node que uses
   clean: true, // Limpia la carpeta dist antes de cada build
   unbundle: false, // Une los archivos para evitar el error de "directory import"
   skipNodeModulesBundle: true, // No intentes resolver node_modules
