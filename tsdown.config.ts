5ff783c1c3ec9267f94a42b7e803273a3ef4bcd5
MarcoCaamal <caamalmarco99@gmail.com>
2026-02-12 11:55:53 -0500

    chore(build): add typecheck and tidy build scripts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/index.ts'],
-  target: 'node24', // O la versión de Node que uses
-  clean: true, // Limpia la carpeta dist antes de cada build
-  unbundle: false, // Une los archivos para evitar el error de "directory import"
-  skipNodeModulesBundle: true, // No intentes resolver node_modules
+  target: 'node24',
+  clean: true,
+  unbundle: false,
+  skipNodeModulesBundle: true,
   alias: {
-    '@': './src' // Mapea tu alias de TS aquí también
+    '@': './src'
   },
-  minify: true, // Minifica el código de salida
+  minify: true,
   platform: 'node'
 })
